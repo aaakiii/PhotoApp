71b3154d7cd82c2864207a7837f003f974aa0468
aaakiii <a.okada3@gmail.com>
2017-11-03 09:45:58 -0700

    connected Photo view and  Map  view

--- PhotoApp/PhotoGalleryViewController.swift
+++ PhotoApp/PhotoGalleryViewController.swift
@@ -16,8 +16,10 @@ UICollectionViewDataSource, UICollectionViewDelegate {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
-//        let nib = UINib(nibName:"PhotoCollectionViewCell", bundle:nil)
-//        collectionView.register(nib, forCellWithReuseIdentifier:"Cell")
+        searchBar.text = ""
+//        searchBar.delegate = self
+//        searchBar.enablesReturnKeyAutomatically = false
+
         getImages()
         
     }
@@ -30,31 +32,30 @@ UICollectionViewDataSource, UICollectionViewDelegate {
         photoCell.setImage(photo:photos[indexPath.row])
         return photoCell
     }
-    func collectionVIew(_ collectionView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Search and set recipeData from [indexPath.row]
-        selectedPhoto = Photo(title: photos[indexPath.row].title, imageByUrl:
-            photos[indexPath.row].imageByUrl, takenDate: photos[indexPath.row].takenDate, latitude: photos[indexPath.row].latitude, longitude: photos[indexPath.row].longitude)
-        selectedPhoto = photos[indexPath.row]
-        if selectedPhoto != nil {
-            // Go to detail view and pass recipe data
-            let storyboard: UIStoryboard = self.storyboard!
-            let nextView = storyboard.instantiateViewController(withIdentifier: "Map") as! MapViewController
-            nextView.selectedPhoto = selectedPhoto
-            self.navigationController?.pushViewController(nextView, animated: true)
-            
+    // when image is selected
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "Map"?:
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first {
+                                let photo = photos[selectedIndexPath.row]
+                                let destinationVC = segue.destination as! MapViewController
+                                destinationVC.selectedPhoto = photo
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
         }
     }
-    
-    
-    
-//    // When cell is selected
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        selectedPhoto = Photo(title: photos[indexPath.row].title, imageByUrl:
-//            photos[indexPath.row].imageByUrl, takenDate: photos[indexPath.row].takenDate, latitude: photos[indexPath.row].latitude, longitude: photos[indexPath.row].longitude)
+
 //
-//        
-//        selectedPhoto = photos[indexPath.row]
+//    func searchBarButtonClicked(_ seachBar: UISearchBar) {
+//        searchBar.endEditing(true)
+//        if()
+//        searchBar.resignFirstResponder()
+//
 //    }
+//
+
     
     
     func getImages(){
@@ -66,51 +67,6 @@ UICollectionViewDataSource, UICollectionViewDelegate {
         }
         
     }
-    
-    //    override func viewDidLoad() {
-    //        super.viewDidLoad()
-    //        collectionView.delegate = self
-    //        collectionView.dataSource = self
-    //        self.title = "Photo Library"
-    //        searchBar.delegate = self
-    //        searchBar.enablesReturnKeyAutomatically = false
-    //        searchBar.text = ""
-    //        searchBar.resignFirstResponder()
-    //
-    //        getImages()
-    //
-    //    }
-    //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    //        return photos.count
-    //    }
-    //
-    //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    //        let photoCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as! PhotoCollectionViewCell
-    //        photoCell.setImage(photo:photos[indexPath.row])
-    //        return photoCell
-    //    }
-    //
-    //
-    //    // When cell is selected
-    //    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    //        selectedPhoto = Photo(title: photos[indexPath.row].title, imageByUrl:
-    //            photos[indexPath.row].imageByUrl, takenDate: photos[indexPath.row].takenDate, latitude: photos[indexPath.row].latitude, longitude: photos[indexPath.row].longitude)
-    //
-    //        selectedPhoto = photos[indexPath.row]
-    //    }
-    //
-    //
-    //    func getImages(){
-    //        Flickr.getImage{ (photos) in
-    //            var imageData = [Photo]()
-    //            imageData = Flickr.photos
-    //            self.photos = imageData
-    //            self.collectionView.reloadData()
-    //        }
-    //
-    //    }
-    //
-    
     
     
 }
